1baf5dd4cda97de76fb3b642cb7cdcb269af0a4a
weidaaaaaaa <yanwli@mail2.sysu.edu.cn>
2019-12-12 21:40:44 +0800

    Refit loadtime polynomial

--- Optimize.py
+++ Optimize.py
@@ -4,19 +4,20 @@ from Time_Prediction import ServerTime, DeviceTime, partition_point_number, mode
 # TODO： B？ 500KB/s for test
 B = 4096000
 
+
 def Optimize(latency_threshold):
     server_time_predictor, device_time_predictor = ServerTime(), DeviceTime()
-    for exit_branch in range(BRANCH_NUMBER-1,-1,-1):
+    for exit_branch in range(BRANCH_NUMBER - 1, -1, -1):
         times = []
         for partition_point in range(partition_point_number[exit_branch]):
             # model load time
-            left_model_size = model_size['branch'+str(exit_branch+1)+'_part'+str(partition_point+1)+'L']
-            right_model_size = model_size['branch'+str(exit_branch+1)+'_part'+str(partition_point+1)+'R']
+            left_model_size = model_size['branch' + str(exit_branch + 1) + '_part' + str(partition_point + 1) + 'L']
+            right_model_size = model_size['branch' + str(exit_branch + 1) + '_part' + str(partition_point + 1) + 'R']
             model_load_time = device_time_predictor.device_model_load(left_model_size) + \
-                              device_time_predictor.device_model_load(right_model_size)
+                              server_time_predictor.server_model_load(right_model_size)
 
             # immediate data size(bits)
-            outputsize=OutputSizeofPartitionLayer.output_size(exit_branch,partition_point)
+            outputsize = OutputSizeofPartitionLayer.output_size(exit_branch, partition_point)
 
             total_time = device_time_predictor.predict_time(exit_branch, partition_point) + \
                          server_time_predictor.predict_time(exit_branch, partition_point) + model_load_time + \
@@ -27,8 +28,10 @@ def Optimize(latency_threshold):
         partition_point = times.index(min(times))
 
         if total_time < latency_threshold:
-            return exit_branch+1, partition_point+1
+            return exit_branch + 1, partition_point + 1
     # if no ep and pp can satisfy latency required then return 1, 1
     return 1, 1
 
 
+if __name__ == '__main__':
+    print(Optimize(0.4))
--- Time_Prediction.py
+++ Time_Prediction.py
@@ -13,7 +13,8 @@ branch3_partition_index = [3, 7, 14]
 # partitiion point number for every branch
 partition_point_number = [2, 2, 3]
 
-branches_info = [(branch1, branch1_partition_index), (branch2, branch2_partition_index), (branch3, branch3_partition_index)]
+branches_info = [(branch1, branch1_partition_index), (branch2, branch2_partition_index),
+                 (branch3, branch3_partition_index)]
 
 # Bytes
 model_size = {
@@ -34,6 +35,7 @@ model_size = {
     'branch3_part3L': 9806701,
     'branch3_part3R': 84083973,
 }
+
 
 ###############################################
 # Mobile device side time prediction class
@@ -110,7 +112,7 @@ class DeviceTime:
         return 0.00020423363723714956 * feature_map_amount + 4.2077298118910815e-11 * compution_each_pixel + 0.025591776113868925
 
     def device_model_load(self, model_size):
-        return 2.2478782378225012e-08 * model_size + 2.527183754111472
+        return 4.558441818370891e-09 * model_size + 0.001395207253916772
 
     # tool
     def predict_time(self, branch_number, partition_point_number):
@@ -120,9 +122,9 @@ class DeviceTime:
         :return:
         '''
         branch_layer, partition_point_index_set = branches_info[branch_number]
-        partition_point =  partition_point_index_set[partition_point_number]
+        partition_point = partition_point_index_set[partition_point_number]
         # layers in partitioned model
-        layers = branch_layer[:partition_point+1]
+        layers = branch_layer[:partition_point + 1]
         time_dict = self.branches[branch_number]
 
         time = 0
@@ -140,12 +142,12 @@ class ServerTime:
             'conv1': self.server_conv(3, (5 * 5 * 3) ** 2 * 64),
             'relu0': self.server_relu(63 * 32 * 32),
             'pool0': self.server_pool(64 * 32 * 32, 64 * 15 * 15),
-            'norm0': self.server_lrn(64* 15 * 15),
+            'norm0': self.server_lrn(64 * 15 * 15),
             'convB1': self.server_conv(64, (3 * 3 * 64) ** 2 * 32),
             'relu1': self.server_relu(32 * 15 * 15),
             'convB2': self.server_conv(32, (3 * 3 * 32) ** 2 * 32),
             'relu2': self.server_relu(32 * 15 * 15),
-            'pool1': self.server_pool(32 * 15 *15, 32 *  7 * 7),
+            'pool1': self.server_pool(32 * 15 * 15, 32 * 7 * 7),
             'linear': self.server_linear(1568, 10),
         }
         self.branch2 = {
@@ -170,7 +172,7 @@ class ServerTime:
             'conv2': self.server_conv(64, (5 * 5 * 64) ** 2 * 192),
             'relu1': self.server_relu(192 * 13 * 13),
             'norm1': self.server_lrn(192 * 13 * 13),
-            'pool1': self.server_pool(192 * 13 * 13 , 192 * 6 * 6),
+            'pool1': self.server_pool(192 * 13 * 13, 192 * 6 * 6),
             'conv3': self.server_conv(192, (3 * 3 * 192) ** 2 * 384),
             'relu2': self.server_relu(384 * 6 * 6),
             'conv4': self.server_conv(384, (3 * 3 * 384) ** 2 * 256),
@@ -185,14 +187,13 @@ class ServerTime:
         }
         self.branches = [self.branch1, self.branch2, self.branch3]
 
-
     def server_lrn(self, data_size):
         return 2.111544033139625e-08 * data_size + 0.0285872721707483
 
     def server_pool(self, input_data_size, output_data_size):
         return -3.08201145e-10 * input_data_size + 1.19458883e-09 * output_data_size - 0.0010152380964514613
 
-    def server_relu(self,input_data_size):
+    def server_relu(self, input_data_size):
         return 2.332339368254984e-09 * input_data_size + 0.005070494191853819
 
     def server_dropout(self, input_data_size):
@@ -206,7 +207,7 @@ class ServerTime:
         return 1.513486447521604e-06 * feature_map_amount + 4.4890001480985655e-12 * compution_each_pixel + 0.009816023641653768
 
     def server_model_load(self, model_size):
-        return 7.073959838308404e-09 * model_size + 3.9334279905883087
+        return 7.753178793348365e-10 * model_size + 0.000678369983568624
 
     # tool
     def predict_time(self, branch_number, partition_point_number):
@@ -251,7 +252,7 @@ class OutputSizeofPartitionLayer:
         :return:unit(bit)
         '''
         branch_layer, partition_point_index_set = branches_info[branch_number]
-        partition_point =  partition_point_index_set[partition_point_number]
+        partition_point = partition_point_index_set[partition_point_number]
         # layers in partitioned model
         layer = branch_layer[partition_point]
         outputsize_dict = cls.branches[branch_number]
